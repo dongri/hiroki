490aee5685f7d262d4f8b114b054fd84df5aff1f
Dongri Jin <dongrify@gmail.com>
2018-08-18 10:11:40 +0900

    add yuya

---
+++ yuya.js
@@ -0,0 +1,40 @@
+var sum = 0;
+function account(principal, startDay, cb){
+  var interest = 0.01;
+	var toDay    = new Date();
+	var days     = parseInt((toDay - startDay)/(60*60*24*1000), 10);
+  var total    = principal + (principal * interest) * days;
+  sum += total;
+  cb(days, total);
+}
+
+account(150, (new Date(2018, 8-1, 10)), function(days, total){
+	document.getElementById('days1-yuya').innerHTML = days;
+  document.getElementById('money1-yuya').innerHTML = total;
+});
+
+document.getElementById('total-yuya').innerHTML = sum;
+
+document.getElementById('start-time-yuya').innerHTML = new Date(2018, 8-1, 10).toLocaleString()
+
+function time(){
+  var now = new Date();
+  var watch = now.toLocaleString();
+  document.getElementById('now-time-yuya').innerHTML = watch;
+  setTimeout("time()", 1000);
+}
+
+time();
+
+$(function(){
+  $("#grid-content-yuya").vgrid({
+    easing: "easeOutQuint",
+    time: 400,
+    delay: 20,
+    fadeIn: {
+      time: 500,
+      delay: 50,
+      wait: 500
+    }
+  });
+});
